610610362761a0067c18f9b1ad2a5fe1b28a08b2
yo999-sir <a113222017@mail.shu.edu.tw>
2025-03-13 13:00:17 +0800

    2025-03-13

---
+++ Program5_9/Program5_9/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Program5_9
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();//創造一個隨機物件，有效範圍在整個Form1
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n1 = rand.Next(1, 7);//產生1~6的亂數,有效範圍在button1_Click方法中
+            int n2 = rand.Next(1, 7);//產生1~6的亂數,有效範圍在button1_Click方法中
+
+            switch (n1)
+            {
+                case 1:
+                    pictureBox1.Image = Properties.Resources._1Die;
+                    break;
+                case 2:
+                    pictureBox1.Image = Properties.Resources._2Die;
+                    break;
+                case 3:
+                    pictureBox1.Image = Properties.Resources._3Die;
+                    break;
+                case 4:
+                    pictureBox1.Image = Properties.Resources._4Die;
+                    break;
+                case 5:
+                    pictureBox1.Image = Properties.Resources._5Die;
+                    break;
+                case 6:
+                    pictureBox1.Image = Properties.Resources._6Die;
+                    break;
+            }
+            switch (n2)
+            {
+                case 1:
+                    pictureBox2.Image = Properties.Resources._1Die;
+                    break;
+                case 2:
+                    pictureBox2.Image = Properties.Resources._2Die;
+                    break;
+                case 3:
+                    pictureBox2.Image = Properties.Resources._3Die;
+                    break;
+                case 4:
+                    pictureBox2.Image = Properties.Resources._4Die;
+                    break;
+                case 5:
+                    pictureBox2.Image = Properties.Resources._5Die;
+                    break;
+                case 6:
+                    pictureBox2.Image = Properties.Resources._6Die;
+                    break;
+
+
+
+            }
+
+        }
+    }
+}
---
+++ Program6_10/Program6_10/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Program6_10
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        string compChoice, myChoice, winner;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getCompChoice();
+        }
+
+        private void getCompChoice()
+        {
+            int n = random.Next(1, 4);
+            switch (n)
+            {
+                case 1:
+                    compChoice = "Rock";
+                    break;
+                case 2:
+                    compChoice = "Paper";
+                    break;
+                case 3:
+                    compChoice = "Scissors";
+                    break;
+            }
+        }
+
+        private void showWinner()
+        {
+            if (myChoice == compChoice)
+                winner = "平手!";
+            else if (myChoice == "Rock" && compChoice == "Scissors")
+                winner = "玩家贏!";
+            else if (myChoice == "Paper" && compChoice == "Rock")
+                winner = "玩家贏!";
+            else if (myChoice == "Scissors" && compChoice == "Paper")
+                winner = "玩家贏!";
+            else
+                winner = "爛!!";
+
+            label1.Text = "玩家選擇: " + myChoice + "\n" +
+                          "電腦選擇: " + compChoice + "\n" +
+                          winner;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myChoice = "Rock";
+            showWinner();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            myChoice = "Paper";
+            showWinner();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            myChoice = "Scissors";
+            showWinner();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            getCompChoice();
+            label1.Text = "";
+        }
+
+
+    }
+}
