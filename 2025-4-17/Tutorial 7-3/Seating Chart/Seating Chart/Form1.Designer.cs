2c8adbd04019f9fb165da603a6adf66a9ee9c6be
yo999-sir <a113222017@mail.shu.edu.tw>
2025-04-17 12:02:37 +0800

    2025-04-17

---
+++ 2025-4-17/Tutorial 7-3/Seating Chart/Seating Chart/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Seating_Chart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.airplanePictureBox = new System.Windows.Forms.PictureBox();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.seatPriceDescriptionLabel = new System.Windows.Forms.Label();
+            this.colPromptLabel = new System.Windows.Forms.Label();
+            this.rowPromptLabel = new System.Windows.Forms.Label();
+            this.colTextBox = new System.Windows.Forms.TextBox();
+            this.rowTextBox = new System.Windows.Forms.TextBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.displayPriceButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.airplanePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // airplanePictureBox
+            // 
+            this.airplanePictureBox.Image = global::Seating_Chart.Properties.Resources.Airplane;
+            this.airplanePictureBox.Location = new System.Drawing.Point(0, 0);
+            this.airplanePictureBox.Name = "airplanePictureBox";
+            this.airplanePictureBox.Size = new System.Drawing.Size(432, 498);
+            this.airplanePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.airplanePictureBox.TabIndex = 0;
+            this.airplanePictureBox.TabStop = false;
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.priceLabel.Location = new System.Drawing.Point(335, 24);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(76, 21);
+            this.priceLabel.TabIndex = 21;
+            this.priceLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // seatPriceDescriptionLabel
+            // 
+            this.seatPriceDescriptionLabel.AutoSize = true;
+            this.seatPriceDescriptionLabel.BackColor = System.Drawing.Color.White;
+            this.seatPriceDescriptionLabel.Location = new System.Drawing.Point(273, 29);
+            this.seatPriceDescriptionLabel.Name = "seatPriceDescriptionLabel";
+            this.seatPriceDescriptionLabel.Size = new System.Drawing.Size(53, 12);
+            this.seatPriceDescriptionLabel.TabIndex = 20;
+            this.seatPriceDescriptionLabel.Text = "座位價格";
+            // 
+            // colPromptLabel
+            // 
+            this.colPromptLabel.AutoSize = true;
+            this.colPromptLabel.BackColor = System.Drawing.Color.White;
+            this.colPromptLabel.Location = new System.Drawing.Point(20, 45);
+            this.colPromptLabel.Name = "colPromptLabel";
+            this.colPromptLabel.Size = new System.Drawing.Size(53, 12);
+            this.colPromptLabel.TabIndex = 19;
+            this.colPromptLabel.Text = "輸入行號";
+            this.colPromptLabel.Click += new System.EventHandler(this.colPromptLabel_Click);
+            // 
+            // rowPromptLabel
+            // 
+            this.rowPromptLabel.AutoSize = true;
+            this.rowPromptLabel.BackColor = System.Drawing.Color.White;
+            this.rowPromptLabel.Location = new System.Drawing.Point(20, 21);
+            this.rowPromptLabel.Name = "rowPromptLabel";
+            this.rowPromptLabel.Size = new System.Drawing.Size(53, 12);
+            this.rowPromptLabel.TabIndex = 18;
+            this.rowPromptLabel.Text = "輸入列號";
+            // 
+            // colTextBox
+            // 
+            this.colTextBox.Location = new System.Drawing.Point(92, 42);
+            this.colTextBox.Name = "colTextBox";
+            this.colTextBox.Size = new System.Drawing.Size(64, 22);
+            this.colTextBox.TabIndex = 17;
+            // 
+            // rowTextBox
+            // 
+            this.rowTextBox.Location = new System.Drawing.Point(92, 18);
+            this.rowTextBox.Name = "rowTextBox";
+            this.rowTextBox.Size = new System.Drawing.Size(64, 22);
+            this.rowTextBox.TabIndex = 16;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(220, 511);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 35);
+            this.exitButton.TabIndex = 23;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // displayPriceButton
+            // 
+            this.displayPriceButton.Location = new System.Drawing.Point(139, 511);
+            this.displayPriceButton.Name = "displayPriceButton";
+            this.displayPriceButton.Size = new System.Drawing.Size(75, 35);
+            this.displayPriceButton.TabIndex = 22;
+            this.displayPriceButton.Text = "顯示價格";
+            this.displayPriceButton.UseVisualStyleBackColor = true;
+            this.displayPriceButton.Click += new System.EventHandler(this.displayPriceButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 558);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.displayPriceButton);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.seatPriceDescriptionLabel);
+            this.Controls.Add(this.colPromptLabel);
+            this.Controls.Add(this.rowPromptLabel);
+            this.Controls.Add(this.colTextBox);
+            this.Controls.Add(this.rowTextBox);
+            this.Controls.Add(this.airplanePictureBox);
+            this.Name = "Form1";
+            this.Text = "Seating Chart";
+            ((System.ComponentModel.ISupportInitialize)(this.airplanePictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox airplanePictureBox;
+        internal System.Windows.Forms.Label priceLabel;
+        internal System.Windows.Forms.Label seatPriceDescriptionLabel;
+        internal System.Windows.Forms.Label colPromptLabel;
+        internal System.Windows.Forms.Label rowPromptLabel;
+        internal System.Windows.Forms.TextBox colTextBox;
+        internal System.Windows.Forms.TextBox rowTextBox;
+        internal System.Windows.Forms.Button exitButton;
+        internal System.Windows.Forms.Button displayPriceButton;
+    }
+}
+
---
+++ 2025-4-17/Tutorial 7-3/Seating Chart/Seating Chart/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seating_Chart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void displayPriceButton_Click(object sender, EventArgs e)
+        {
+            const int ROWS = 6;
+            const int COLS = 4;
+            int row;
+            int col;
+            decimal[,] seatPrices = { {450m, 450m, 450m, 450m},
+                           {425m, 425m, 425m, 425m},
+                           {400m, 400m, 400m, 400m},
+                           {375m, 375m, 375m, 375m},
+                           {375m, 375m, 375m, 375m},
+                           {350m, 350m, 350m, 350m}};
+            if (int.TryParse(rowTextBox.Text, out row))
+            {
+                if (int.TryParse(colTextBox.Text, out col))
+                {
+                    if (row >= 0 && row < ROWS)
+                    {
+                        if (col >= 0 && col < COLS)
+                        {
+                            priceLabel.Text = seatPrices[row, col].ToString();
+                        }
+                        else
+                        {
+                            MessageBox.Show("行號0-3");
+                            colTextBox.Focus();
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("列號0-5");
+                        rowTextBox.Focus();
+                        return;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("輸入有效行號");
+                    colTextBox.Focus();
+                    return;
+                }
+            }
+            else
+            {
+                MessageBox.Show("輸入有效列號");
+                rowTextBox.Focus();
+                return;
+            }
+        }
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            // Close the form.
+            this.Close();
+        }
+
+        private void colPromptLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
